542397321507ae923dc7976fd87cc096ccff4b10
poddarrishi02 <poddarrishi02@gmail.com>
2022-07-01 19:23:02 +0530

    minor changes in comments

--- GameOfGolds.cpp
+++ GameOfGolds.cpp
@@ -7,12 +7,12 @@ along each corridor of the maze there is a bag of gold
 coins. The amount of gold in each bag varies. A noble
 knight, named Sir Paul, will be given the opportunity
 to walk through the maze, picking up bags of gold. He may
-enter the maze only through a door marked �ENTER� and
-exit through another door marked �EXIT.� While in the
+enter the maze only through a door marked ENTER and
+exit through another door marked EXIT. While in the
 maze, he may not retrace his steps. Each corridor of the
 maze has an arrow painted on the wall. Sir Paul may
 only go down the corridor in the direction of the arrow.
-There is no way to traverse a �loop� in the maze. Given a
+There is no way to traverse a loop in the maze. Given a
 map of the maze, including the amount of gold in and the
 direction of each corridor, describe an algorithm
 to help Sir Paul pick up the most gold.
@@ -22,8 +22,8 @@ to help Sir Paul pick up the most gold.
 Input Format
 Size n, denoting number of edges
 array1->weights of edges
-array2->initial node for array1[i�th]
-array3->final node for array1[i�th]
+array2->initial node for array1[ith]
+array3->final node for array1[ith]
 Integers x,y->Entry node, exit node
 Constraints
 array1[i]>=0
